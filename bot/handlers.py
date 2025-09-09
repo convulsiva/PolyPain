b0a0a747c0ae37312d17ec92311ef33be7478fd3
convulsiva <firelifeeeeee@gmail.com>
2025-09-10 00:00:28 +0300

    feat: add user storage with JSON

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,11 +1,12 @@
-from telebot import TeleBot
 from telebot.types import Message
+from services.user_storage import add_user
 
-def register_handlers(bot: TeleBot):
+def register_handlers(bot):
     @bot.message_handler(commands=["start"])
     def start_handler(message: Message):
-        bot.reply_to(message, "Привет! Я твой бот ")
+        add_user(message.chat.id, message.from_user.username, message.from_user.first_name)
+        bot.reply_to(message, "Привет! Ты добавлен в базу ✅")
 
     @bot.message_handler(commands=["ping"])
     def ping_handler(message: Message):
-        bot.reply_to(message, "pong")
+        bot.reply_to(message, "pong 🏓")
