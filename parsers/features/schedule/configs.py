ae465e49171798376f103e09e5d208c337dae54e
Egor <shishkin.es@edu.spbstu.ru>
2025-10-16 23:49:36 +0300

    feat: draft parser class

--- parsers/features/schedule/configs.py
+++
@@ -1,9 +0,0 @@
-from furl import furl
-from infra.client import configs
-
-SCHEDULE_CONFIG = configs.ConfigBox(
-    client=configs.ClientConfig(furl("https://ruz.spbstu.ru/"), "RuzSPbPU"),
-    net=configs.NetConfig(),
-    cache=configs.CacheConfig(enabled=True, automatic_prune_cache=True, ttl=10),
-    cookie=configs.CookieConfig(),
-)
--- parsers/features/schedule/parser.py
+++ parsers/features/schedule/parser.py
@@ -1,7 +1,3 @@
-import datetime as dt
-from functools import wraps
-
-from dtos import DayDTO, WeekScheduleDTO
 from infra.client import Client
 from infra.client.configs import ConfigBox
 
@@ -16,23 +12,7 @@ from scrapers import (
 class ScheduleParser:
     def __init__(self, configs: ConfigBox) -> None:
         self._client = Client(configs)
-        self._group_id_scraper = GroupIdScraper(self._client)
-        self._group_existence_scraper = GroupExistenceScraper(self._client)
-        self._week_schedule_scraper = WeekScheduleScraper(self._client)
-        self._daily_schedule_scraper = DailyScheduleScraper(self._client)
-
-    @wraps(GroupIdScraper.__call__)
-    def get_group_id(self, name: str) -> int:
-        return self._group_id_scraper(name)
-
-    @wraps(GroupExistenceScraper.__call__)
-    def group_exist(self, name: str, strict: bool = True) -> bool | tuple[str, ...]:
-        return self._group_existence_scraper(name, strict)
-
-    @wraps(WeekScheduleScraper.__call__)
-    def get_week_schedule(self, name: str, date: str | dt.date | None = None) -> WeekScheduleDTO:
-        return self._week_schedule_scraper(name, date)
-
-    @wraps(DailyScheduleScraper.__call__)
-    def get_daily_schedule(self, name: str, date: str | dt.date) -> DayDTO:
-        return self._daily_schedule_scraper(name, date)
+        self.get_group_id = GroupIdScraper(self._client)
+        self.group_exist = GroupExistenceScraper(self._client)
+        self.get_week_schedule = WeekScheduleScraper(self._client)
+        self.get_daily_schedule = DailyScheduleScraper(self._client)
