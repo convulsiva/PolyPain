60f38ccbacc2c2782ebd02858e093190bf978f05
Egor <shishkin.es@edu.spbstu.ru>
2025-10-20 01:06:01 +0300

    feat: add SongLyricsParser

---
+++ parsers/features/song_lyrics/parser.py
@@ -0,0 +1,10 @@
+from infra.client import Client, configs
+
+from .scrapers import SearchSongsScraper, SongLyricsScraper
+
+
+class SongLyricsParser:
+    def __init__(self, config_box: configs.ConfigBox) -> None:
+        self._client = Client(config_box)
+        self.get_song_info = SearchSongsScraper(self._client)
+        self.get_song_lyrics = SongLyricsScraper(self._client)
