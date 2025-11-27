5e5288c064ed7b4adadfcae210729676c8cd29a8
egor <egorka.shishkin.06@gmail.com>
2025-11-28 00:22:24 +0300

    feat: add BaseParser class with set_db_path method

--- parsers/features/song_lyrics/parser.py
+++ parsers/features/song_lyrics/parser.py
@@ -1,9 +1,10 @@
-from ...infra.client import Client, configs
+from ...infra.client import configs
+from ..base_parser import BaseParser
 from .scrapers import SearchSongsScraper, SongLyricsScraper
 
 
-class SongLyricsParser:
+class SongLyricsParser(BaseParser):
     def __init__(self, config_box: configs.ConfigBox) -> None:
-        self._client = Client(config_box)
+        super().__init__(config_box)
         self.search_songs = SearchSongsScraper(self._client)
         self.get_song_lyrics = SongLyricsScraper(self._client)
